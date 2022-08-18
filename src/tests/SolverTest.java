e7e76a84f26188527ec8d5f0658c75e714b1dbf3
Cattherinne <ekaterina.vasilko@mail.ru>
2022-08-18 21:05:32 +0500

    Автоматические тесты на кубе размерности 3

--- src/tests/SolverTest.java
+++ src/tests/SolverTest.java
@@ -17,4 +17,77 @@ public class SolverTest {
         solver.calculateSpectrum();
         Assert.assertEquals("0, 1, 1", solver.toString());
     }
+
+    @Test
+    public void check3D() {
+        var solver = new Solver("00000000");
+        solver.calculateSpectrum();
+        Assert.assertEquals("0, 0, 0, 0", solver.toString());
+        solver = new Solver("00000001");
+        solver.calculateSpectrum();
+        Assert.assertEquals("0, 1, 2, 3", solver.toString());
+        solver = new Solver("00000011");
+        solver.calculateSpectrum();
+        Assert.assertEquals("0, 1, 2, 0", solver.toString());
+        solver = new Solver("00000110");
+        solver.calculateSpectrum();
+        Assert.assertEquals("0, 1, 2, 1", solver.toString());
+        solver = new Solver("00000111");
+        solver.calculateSpectrum();
+        Assert.assertEquals("0, 1, 2, 0", solver.toString());
+        solver = new Solver("00001111");
+        solver.calculateSpectrum();
+        Assert.assertEquals("0, 1, 0, 0", solver.toString());
+        solver = new Solver("00010110");
+        solver.calculateSpectrum();
+        Assert.assertEquals("0, 1, 2, 1", solver.toString());
+        solver = new Solver("00010111");
+        solver.calculateSpectrum();
+        Assert.assertEquals("0, 1, 2, 0", solver.toString());
+        solver = new Solver("00011000");
+        solver.calculateSpectrum();
+        Assert.assertEquals("0, 1, 2, 2", solver.toString());
+        solver = new Solver("00011001");
+        solver.calculateSpectrum();
+        Assert.assertEquals("0, 1, 2, 1", solver.toString());
+        solver = new Solver("00011011");
+        solver.calculateSpectrum();
+        Assert.assertEquals("0, 1, 2, 0", solver.toString());
+        solver = new Solver("00011110");
+        solver.calculateSpectrum();
+        Assert.assertEquals("0, 1, 2, 1", solver.toString());
+        solver = new Solver("00011111");
+        solver.calculateSpectrum();
+        Assert.assertEquals("0, 1, 2, 0", solver.toString());
+        solver = new Solver("00111100");
+        solver.calculateSpectrum();
+        Assert.assertEquals("0, 1, 1, 0", solver.toString());
+        solver = new Solver("00111101");
+        solver.calculateSpectrum();
+        Assert.assertEquals("0, 1, 1, 0", solver.toString());
+        solver = new Solver("00111111");
+        solver.calculateSpectrum();
+        Assert.assertEquals("0, 1, 0, 0", solver.toString());
+        solver = new Solver("01101001");
+        solver.calculateSpectrum();
+        Assert.assertEquals("0, 1, 1, 1", solver.toString());
+        solver = new Solver("01101011");
+        solver.calculateSpectrum();
+        Assert.assertEquals("0, 1, 1, 1", solver.toString());
+        solver = new Solver("01101111");
+        solver.calculateSpectrum();
+        Assert.assertEquals("0, 1, 1, 0", solver.toString());
+        solver = new Solver("01111110");
+        solver.calculateSpectrum();
+        Assert.assertEquals("0, 1, 0, 0", solver.toString());
+        solver = new Solver("01111111");
+        solver.calculateSpectrum();
+        Assert.assertEquals("0, 1, 0, 0", solver.toString());
+        solver = new Solver("11111111");
+        solver.calculateSpectrum();
+        Assert.assertEquals("0, 0, 0, 0", solver.toString());
+        solver = new Solver("11111111");
+        solver.calculateSpectrum();
+        Assert.assertNotEquals("0, 0, 0, 1", solver.toString());
+    }
 }
